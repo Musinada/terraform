9acee6c47cdea752108517c0f06dd25a8cb7b943
Anil Kumar Musinada <121026687+Musinada@users.noreply.github.com>
2023-05-30 21:57:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,20 +16,15 @@ provider "aws" {
   region  = var.region
 }
 
-#module "aws_security_group" {
-#  source      = "terraform-aws-modules/security-group/aws"
-#  name = "k8s-cluster-sg"
-#}
-
 resource "aws_instance" "controller_ubuntu" {
   ami                         = var.ubuntu_ami_id
   instance_type               = "t2.micro"
   associate_public_ip_address = true
   subnet_id = var.subnet
-  key_name                    = "ssh-key"
+  key_name                    = "anil"
   root_block_device {
     volume_type           = var.volume_type
-    volume_size           = 
+    volume_size           = 60
     delete_on_termination = true
     encrypted = true
   }
